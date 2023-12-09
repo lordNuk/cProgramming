1134c5a6105538ecd9490e3b73983446f069a81c
lordNuk <lordnuk501@gmail.com>
2023-12-09 11:39:00 +0530

    added git ignore file with test.c & a.out

--- chapter-3/E/f.c
+++ chapter-3/E/f.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/*
 int main() {
   int i, j;
   for(i = 0; i < 7; i++) {
@@ -18,11 +19,29 @@ int main() {
   }
   return 0;
 }
+*/
 
+int main() {
+  int i, j;
+  for(i = 0; i < 7; i++) {
+    for(j = 0; j < 13; j++) {
+      if(j-6-i < 0 && 6-i-j < 0) {
+        printf("  ");
+        continue;
+      }
+      if(j < 7) {
+        printf("%c ", 'A' + j);
+      } else {
+        printf("%c ", 'F' - j%7);
+      }
+    }
+    printf("\n");
+  }
+  return 0;
+}
 
 
  
-
 
 
 
